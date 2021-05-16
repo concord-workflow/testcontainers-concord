cb85852a594b076292aafda255481012666cf58c
Brigadirenko Yury <ybrigo@gmail.com>
2021-05-16 16:39:19 +0300

    localmode: up for agent api token

--- testcontainers-concord-core/src/main/java/ca/ibodrov/concord/testcontainers/LocalConcordEnvironment.java
+++ testcontainers-concord-core/src/main/java/ca/ibodrov/concord/testcontainers/LocalConcordEnvironment.java
@@ -49,6 +49,7 @@ public class LocalConcordEnvironment implements ConcordEnvironment {
 
     private final GenericContainer<?> db;
     private final String apiToken;
+    private final String agentToken;
     private final String pathToRunnerV1;
     private final String pathToRunnerV2;
     private final boolean startAgent;
@@ -75,6 +76,7 @@ public class LocalConcordEnvironment implements ConcordEnvironment {
         }
 
         this.apiToken = Utils.randomToken();
+        this.agentToken = Utils.randomToken();
 
         this.pathToRunnerV1 = opts.pathToRunnerV1();
         this.pathToRunnerV2 = opts.pathToRunnerV2();
@@ -160,6 +162,7 @@ public class LocalConcordEnvironment implements ConcordEnvironment {
         s = s.replaceAll("SERVER_PORT", String.valueOf(apiPort));
         s = s.replaceAll("DB_URL", "jdbc:postgresql://localhost:" + db.getFirstMappedPort() + "/postgres");
         s = s.replaceAll("API_TOKEN", apiToken);
+        s = s.replaceAll("AGENT_TOKEN", agentToken);
         s = s.replaceAll("JAVA_CMD", getJavaCmd());
         if (pathToRunnerV1 != null) {
             s = s.replaceAll("RUNNER_V1_PATH", pathToRunnerV1);
